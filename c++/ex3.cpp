71fc75c9df65f7e879dd4bddace22c4433a1165b
Minori KAMAKURA <23238116@edu.cc.saga-u.ac.jp>
2026-06-30 23:08:49 +0900

    a

---
+++ c++/ex3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << 100 * (100 + 1) / 2 << endl;
+}
---
+++ c++/ex5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // ここにプログラムを追記
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
